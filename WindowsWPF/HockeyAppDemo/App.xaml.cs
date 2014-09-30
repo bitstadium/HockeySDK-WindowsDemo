75a86cd70bb84bec1d8be38351d9aae93355f7b9
Andreas Moosbrugger <am@mogular.com>
2014-09-30 21:33:33 +0200

    First draft of WPF DemoApp

---
+++ WindowsWPF/HockeyAppDemo/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using HockeyApp;
+
+namespace HockeyAppDemoWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            HockeyClient.Current.Configure(DemoConstants.YOUR_APP_ID);
+            await HockeyClient.Current.SendCrashesAsync(true);
+            await HockeyClient.Current.CheckForUpdatesAsync(true, () =>
+            {
+                if (Application.Current.MainWindow != null) { Application.Current.MainWindow.Close(); }
+                return true;
+            });
+        }
+    }
+}
---
+++ WindowsWPF/HockeyAppDemo/DemoConstants.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HockeyAppDemoWPF
+{
+    public partial class DemoConstants
+    {
+        //TODO add your app id and secret here !
+
+        public static string YOUR_APP_ID = "your_app_id";
+        public static string YOUR_APP_SECRET = "your_app_secret";
+
+        public static string USER_EMAIL = "mail@example.com";
+        public static string USER_NAME = "John Smith";
+
+    }
+}
