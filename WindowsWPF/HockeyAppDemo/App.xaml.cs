490f6b00591bff5e92c2299fdf6393362b4f72a8
Mike Smacinih <mihailsm@microsoft.com>
2015-10-21 11:24:52 -0700

    Update to HockeySDK 3.0

--- WindowsWPF/HockeyAppDemo/App.xaml.cs
+++ WindowsWPF/HockeyAppDemo/App.xaml.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using HockeyApp;
+using Microsoft.HockeyApp;
 using System.Diagnostics;
 
 namespace HockeyAppDemoWPF
--- WindowsWPF/HockeyAppDemo/MainWindow.xaml.cs
+++ WindowsWPF/HockeyAppDemo/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using HockeyApp;
+﻿using Microsoft.HockeyApp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
