62224f0c0be4626e2d060122f86d40c511e010f1
Mike Smacinih <mihailsm@microsoft.com>
2015-11-02 12:48:32 -0800

    Created HockeyAppDemo application for UWP

---
+++ UWP/HockeyAppDemoUwp/cs/SampleConfiguration.cs
@@ -0,0 +1,33 @@
+//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using System.Collections.Generic;
+using Windows.UI.Xaml.Controls;
+
+namespace HockeyAppDemo
+{
+    public partial class MainPage : Page
+    {
+        public const string FEATURE_NAME = "Universal Windows Platform";
+
+        List<Scenario> scenarios = new List<Scenario>
+        {
+            new Scenario() { Title = "Crashes", ClassType = typeof(Scenario1_Crashes) },
+        };
+    }
+
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public Type ClassType { get; set; }
+    }
+}
---
+++ UWP/HockeyAppDemoUwp/cs/Scenario1_Crashes.xaml.cs
@@ -0,0 +1,32 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using Windows.UI.Xaml.Controls;
+
+namespace HockeyAppDemo
+{
+    public sealed partial class Scenario1_Crashes : Page
+    {
+        public Scenario1_Crashes()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// This is the click handler for the 'Display' button.
+        /// </summary>
+        private void ExceptionButton_Click()
+        {
+            throw new Exception("TestException from DemoApp");
+        }
+    }
+}
