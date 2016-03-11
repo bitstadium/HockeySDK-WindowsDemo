f5b60ac027756a1ee73cbbb3529ce288abfdeaae
Mike Smacinih <mihailsm@microsoft.com>
2016-03-11 10:37:39 -0800

    Upgrade to HockeySDK.UWP 3.1.1-build59744

--- UWP/HockeyAppDemoUwp/cs/App.xaml.cs
+++ UWP/HockeyAppDemoUwp/cs/App.xaml.cs
@@ -30,7 +30,17 @@ namespace HockeyAppDemo
         /// </summary>
         public App()
         {
-            Microsoft.HockeyApp.HockeyClient.Current.Configure(DemoConstants.YOUR_APP_ID);
+            Microsoft.HockeyApp.HockeyClient.Current.Configure(DemoConstants.YOUR_APP_ID,
+                new Microsoft.HockeyApp.TelemetryConfiguration()
+                {
+                    // DescriptionLoader callback allows user to provide additional exception related in the context of a crash.
+                    // In this case we are adding exception HResult
+                    DescriptionLoader = (Exception ex) => 
+                    {
+                        return "Exception HResult: " + ex.HResult.ToString();
+                    }
+                });
+
             this.InitializeComponent();
             this.Construct();
         }
