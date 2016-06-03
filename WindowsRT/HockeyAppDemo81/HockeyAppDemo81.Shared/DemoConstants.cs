abdf27fbea7c39c23d60dab059edad87e7f312af
Mike Smacinih <mihailsm@microsoft.com>
2016-06-03 15:16:35 -0700

    Upgrade to HockeySDK 4.1.57036

--- WindowsRT/HockeyAppDemo81/HockeyAppDemo81.Shared/DemoConstants.cs
+++ WindowsRT/HockeyAppDemo81/HockeyAppDemo81.Shared/DemoConstants.cs
@@ -1,18 +1,18 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace HockeyAppDemo81
-//{
-//    public partial class DemoConstants
-//    {
-//        //TODO add your app id and secret here !
+namespace HockeyAppDemo81
+{
+    public partial class DemoConstants
+    {
+        //TODO add your app id and secret here !
 
-//        public static string YOUR_APP_ID = "343585b9d9a349d592dad7605a38a482";
-//        public static string YOUR_APP_SECRET = "001db4bf0f37f17a64e64fe6eba41d53";
+        public static string YOUR_APP_ID = "343585b9d9a349d592dad7605a38a482";
+        public static string YOUR_APP_SECRET = "001db4bf0f37f17a64e64fe6eba41d53";
 
-//        public static string USER_EMAIL = "mail@example.com";
-//        public static string USER_NAME = "John Smith";
+        public static string USER_EMAIL = "mail@example.com";
+        public static string USER_NAME = "John Smith";
 
-//    }
-//}
+    }
+}
