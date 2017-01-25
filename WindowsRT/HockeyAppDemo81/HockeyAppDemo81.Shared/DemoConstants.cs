744496af09d4d2fdabd03ff6dba0e5e111edfe65
Alexander Chocron <alchocro@microsoft.com>
2017-01-25 09:46:43 -0800

    Update WindowsRT app

--- WindowsRT/HockeyAppDemo81/HockeyAppDemo81.Shared/DemoConstants.cs
+++ WindowsRT/HockeyAppDemo81/HockeyAppDemo81.Shared/DemoConstants.cs
@@ -6,12 +6,12 @@ namespace HockeyAppDemo81
 {
     public partial class DemoConstants
     {
-        //TODO add your app id and secret here !
+        //TODO add your app id and secret here!
 
         // HockeyAppDemo_WinRT.
-        public static string YOUR_APP_ID = "343585b9d9a349d592dad7605a38a482";
+        public static string YOUR_APP_ID = "yourappid";
 
-        public static string YOUR_APP_SECRET = "001db4bf0f37f17a64e64fe6eba41d53";
+        public static string YOUR_APP_SECRET = "yourappsecret";
 
         public static string USER_EMAIL = "mail@example.com";
         public static string USER_NAME = "John Smith";
