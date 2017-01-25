4f7d532d11aa7aa7dd43f60ce5e7cfb19c7ee3ed
Alexander Chocron <alchocro@microsoft.com>
2017-01-25 09:54:47 -0800

    Update WPF app

--- WindowsWPF/HockeyAppDemo/DemoConstants.cs
+++ WindowsWPF/HockeyAppDemo/DemoConstants.cs
@@ -4,8 +4,8 @@
     {
         //TODO add your app id and secret here !
 
-        public static string YOUR_APP_ID = "0e900cf2b7ab4ea792349ae6ae62f686";
-        public static string YOUR_APP_SECRET = "a70fae2a2d159f64f3c1d6df9d7f21aa";
+        public static string YOUR_APP_ID = "yourappid";
+        public static string YOUR_APP_SECRET = "yourappsecret";
 
         public static string USER_EMAIL = "mail@example.com";
         public static string USER_NAME = "John Smith";
